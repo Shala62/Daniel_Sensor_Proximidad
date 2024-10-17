56cab9c8e32a6e310a561e8950789478275f6d13
Daniel Soto <danielsoto200462@gmail.com>
2024-10-16 23:05:30 -0300

    Se sube el Proyecto de Sensor de Proximidad

---
+++ app/src/main/java/com/example/daniel_sensor_proximidad/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.daniel_sensor_proximidad;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private EditText et1;
+    private LinearLayout linMain;
+    private SensorManager sm;
+    private Sensor sensor;
+    private SensorEventListener SEL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        et1 = findViewById(R.id.et1);
+        linMain = findViewById(R.id.linMain);
+        sm  = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sm.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        String text = String.valueOf(event.values[0]);
+        et1.setText(text);
+        float valor = Float.parseFloat(text);
+        if(valor == 0){
+            linMain.setBackgroundColor(Color.RED);
+        }else{
+            linMain.setBackgroundColor(Color.GREEN);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
